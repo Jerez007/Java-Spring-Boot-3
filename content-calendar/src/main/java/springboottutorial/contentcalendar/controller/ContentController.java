6bb54bd62a47bec93996b6fc33cd27658a973ed3
JJ <jerez.solis@gmail.com>
2023-06-10 13:29:59 -0700

    added post and get methods for API

--- content-calendar/src/main/java/springboottutorial/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/springboottutorial/contentcalendar/controller/ContentController.java
@@ -1,10 +1,37 @@
 package springboottutorial.contentcalendar.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import springboottutorial.contentcalendar.model.Content;
+import springboottutorial.contentcalendar.repository.ContentCollectionRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
 public class ContentController {
+
+    private final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("")
+    public List<Content> findAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Content findById(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found!"));
+    }
+
+    @PostMapping("")
+    public void create(@RequestBody Content content) {
+        repository.save(content);
+    }
 
 }
--- content-calendar/src/main/java/springboottutorial/contentcalendar/repository/ContentCollectionRepository.java
+++ content-calendar/src/main/java/springboottutorial/contentcalendar/repository/ContentCollectionRepository.java
@@ -1,8 +1,12 @@
 package springboottutorial.contentcalendar.repository;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Repository;
 import springboottutorial.contentcalendar.model.Content;
+import springboottutorial.contentcalendar.model.Status;
+import springboottutorial.contentcalendar.model.Type;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -10,16 +14,36 @@ import java.util.Optional;
 // Keeps a collection of content in-memory to test out application
 @Repository
 public class ContentCollectionRepository {
-    private final List<Content> content = new ArrayList<>();
+    private final List<Content> contentList = new ArrayList<>();
 
     public ContentCollectionRepository() {
     }
 
     public List<Content> findAll() {
-        return content;
+        return contentList;
     }
 
     public Optional<Content> findById(Integer id) {
-        return content.stream().filter(c --> c.id().equals(id)).findFirst();
+        return contentList.stream().filter(c -> c.id().equals(id)).findFirst();
     }
+
+
+    public void save(Content content) {
+        contentList.add(content);
+    }
+
+    @PostConstruct
+    private void init() {
+        Content content = new Content(1,
+                "My first Blog Post",
+                "My first blog post",
+                Status.IDEA,
+                Type.ARTICLE,
+                LocalDateTime.now(),
+                null,
+                "");
+
+        content.add(content);
+    }
+
 }
