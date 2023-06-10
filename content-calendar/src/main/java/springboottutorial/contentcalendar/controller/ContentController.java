d7c839c12f82ddedb1359b27d83216207f59e25f
JJ <jerez.solis@gmail.com>
2023-06-10 16:06:26 -0700

    added validation by adding spring-boot-starter-validation dependency in pom.xml

--- content-calendar/src/main/java/springboottutorial/contentcalendar/controller/ContentController.java
+++ content-calendar/src/main/java/springboottutorial/contentcalendar/controller/ContentController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin // allows cors
 public class ContentController {
 
     private final ContentCollectionRepository repository;
