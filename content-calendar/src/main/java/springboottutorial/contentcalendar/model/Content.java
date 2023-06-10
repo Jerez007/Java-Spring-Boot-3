f94b667be7fde77c8c0298d43e052f1972b42f1d
JJ <jerez.solis@gmail.com>
2023-06-10 11:57:53 -0700

    added contentcollectionrepository.java

--- content-calendar/src/main/java/springboottutorial/contentcalendar/model/Content.java
+++ content-calendar/src/main/java/springboottutorial/contentcalendar/model/Content.java
@@ -1,4 +1,15 @@
 package springboottutorial.contentcalendar.model;
 
-public record Content() {
+import java.time.LocalDateTime;
+
+public record Content(
+        Integer id,
+        String title,
+        String desc,
+        Status status,
+        Type contentType,
+        LocalDateTime dateCreated,
+        LocalDateTime dateUpdated,
+        String url
+) {
 }
---
+++ content-calendar/src/main/java/springboottutorial/contentcalendar/repository/ContentCollectionRepository.java
@@ -0,0 +1,25 @@
+package springboottutorial.contentcalendar.repository;
+
+import org.springframework.stereotype.Repository;
+import springboottutorial.contentcalendar.model.Content;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+// Keeps a collection of content in-memory to test out application
+@Repository
+public class ContentCollectionRepository {
+    private final List<Content> content = new ArrayList<>();
+
+    public ContentCollectionRepository() {
+    }
+
+    public List<Content> findAll() {
+        return content;
+    }
+
+    public Optional<Content> findById(Integer id) {
+        return content.stream().filter(c --> c.id().equals(id)).findFirst();
+    }
+}
